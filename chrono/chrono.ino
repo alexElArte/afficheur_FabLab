9a6f6170c7a75580423faa3f9b4c628e2e5fa72c
alex_el.arte <77207986+alexElArte@users.noreply.github.com>
2022-03-18 21:18:19 +0100

    Minor bug fixed
    Prochainement le renouvellement du comptage qui commence à se tromper.

--- chrono/chrono.ino
+++ chrono/chrono.ino
@@ -9,9 +9,9 @@ void loop() {
   // millis() -> max ~50d
   uint32_t centi = millis() / 10;
   uint32_t seconds = centi / 100;
-  uint32_t minutes = centi / (60*100);
-  uint32_t hours = centi / (60*60*100);
-  uint32_t days = centi / (24*60*60*100);
+  uint32_t minutes = seconds / (60);
+  uint32_t hours = seconds / (60*60);
+  uint32_t days = seconds / (24*60*60);
 
 
   if (minutes < 1) {              // 00s 00ms - 59s 99ms
@@ -20,12 +20,12 @@ void loop() {
     set_digit(2, (centi/10)%10);   // XXAX
     set_digit(3, (centi%10));      // XXXA
   } else if (hours < 1) {        // 01min 00s - 59min 59s
-    set_digit(0, (minutes/10));    // AXXX
+    set_digit(0, (minutes/10)%10);    // AXXX
     set_digit(1, (minutes%10));    // XAXX
     set_digit(2, (seconds/10)%6);  // XXAX
     set_digit(3, (seconds%10));    // XXXA
   } else if (days < 1) {         // 01h 00min - 23h 59min
-    set_digit(0, (hours/10));      // AXXX
+    set_digit(0, (hours/10)%10);      // AXXX
     set_digit(1, (hours%10));      // XAXX
     set_digit(2, (minutes/10)%6);  // XXAX
     set_digit(3, (minutes%10));    // XXXA
